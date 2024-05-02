b93f2fc05bcb53ab4fc0815a0d20ea745ffa3019
Arshad <arshadarmer@gmail.com>
2024-05-02 07:40:45 +0530

    created train test directory and semi ingestion component

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,15 +13,18 @@ import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
+
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass #The dataclasses module in Python provides a decorator from 3.7 python
 # and functions for automatically generates special methods
-# such as __init__() or constructor method of a classor instance method or initializer method 
-# and __repr__() or __eq__()to user-defined classes, making it easier to create classes that primarily store data.
+# such as __init__() or 'constructor method' of a class/'instance method'/'initializer method' 
+# and __repr__() or __eq__() to user-defined classes,
+# making it easier to create classes that primarily store data.
 
 @dataclass
 class DataingestionConfig(): #any input i require i ll put it here in dataingestion config
-    train_data_path: str = os.path.join('artifacts','train.csv') #we are using dataclasses module,so little bit different we dont need to initialize the __init_
+    train_data_path: str = os.path.join('artifacts','train.csv') #we are using dataclasses module,so little bit different we dont need to initialize the __init_() Constructor
     test_data_path: str = os.path.join('artifacts','test.csv')
     raw_data_path: str = os.path.join('artifacts','raw_data.csv')
     #Note:@dataclass does not handle not None Part =>>VERY VERY IMPORTANT
@@ -47,4 +50,33 @@ class DataingesttionConfig_():
 class Dataingestion():
     def __init__(self):
         self.ingestion_config=DataingestionConfig() #self.ingestion_config holds all three paths: train_data_path, test_data_path, and raw_data_path.
+        #self.ingestion_config is similiar to variable called as "INSTANCE"
+        #self.ingestion_config is like a variable specific to each instance of the Dataingestion class,
+        # holding a reference to an object of type DataingestionConfig.
+        
+    def initiate_data_ingestion(self):
+        logging.info("Entered the Data Ingestion Method or Component")
+        
+        try:
+            df=pd.read_csv('stud.csv')
+            logging.info('Read the dataset as Dataframe')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) #to store raw data
+            
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) #to store train data
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True) #to store test data
+            
+            logging.info("Ingestion of data Completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path #have doubt y not returning raw_data_path see how return works here
+            )
+            
+        except Exception as e:
+            raise CustomException(e,sys) #SEE EXCEPTION HOW IT WORKS
 
+            
+            
