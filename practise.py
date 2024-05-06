df95f3d6b31adbc7ec90d07dcd612f07ded1c482
Arshad <arshadarmer@gmail.com>
2024-05-06 09:54:11 +0530

    Check basic Things with practise.py

--- practise.py
+++ practise.py
@@ -6,3 +6,27 @@ print(ans)
 c=10.6
 print(m.floor(c))
 print(m.ceil(c))
+#To print Arshad Rocks Rocks Rocks Rocks 5 times
+i=1
+while i<=5:
+    print("Arshad",end=" ")
+    i+=1
+    j=1
+    while j<=4:
+        print("Rocks",end=" ")
+        j+=1
+    print()
+#Stock
+stock=6
+i=1
+while i<=6:
+    if i>6:
+        break
+    else:
+        print(i,":Candy")
+        i+=1
+#return Statement
+def add_sub(x,y):
+    return x+y,x-y #returning multiple values in a statement
+add,sub=add_sub(10,5)
+print(add,"\n",sub)
